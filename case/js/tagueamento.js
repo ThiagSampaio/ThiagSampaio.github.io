1eca6cca0623c6311091893fc6f753267e25c8dc
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-10 22:33:21 -0300

    Adiciona autoria

--- case/js/tagueamento.js
+++ case/js/tagueamento.js
@@ -5,6 +5,8 @@
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
 
+//Thiago Sampaio 10/01/2022 
+
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments)
